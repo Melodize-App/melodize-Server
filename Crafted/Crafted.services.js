c692efdb9b904d47da1019df66ebe5e2f9cdea3e
RefaelBZ <refael747@gmail.com>
2024-06-13 21:17:06 +0300

    gitignore

---
+++ Crafted/Crafted.services.js
@@ -0,0 +1,34 @@
+const listenedController=require("./Crafted.controller")
+
+
+
+
+async function addListenedSong(song, userId) {
+    try {
+        let songExists = await listenedController.readOne(song.video_id);
+
+        if (songExists) {
+            // השתמש ב-$inc להגדלת ה-playCount
+            return await listenedController.update(song.video_id, { $inc: { playCount: 1 } });
+        } else {
+            // אם השיר לא קיים, הוסף אותו עם playCount התחלתי של 1
+            return await listenedController.create({ ...song, User: userId, playCount: 1 });
+        }
+    } catch (error) {
+        console.error('Error in addListenedSong:', error);
+        return { success: false, message: "An error occurred while adding or updating the song", error: error.message };
+    }
+}
+
+
+
+
+async function getcraftedSongs(userId) {
+    return await listenedController.readByUser(userId);
+  }
+
+
+
+module.exports = {addListenedSong,getcraftedSongs};
+
+
