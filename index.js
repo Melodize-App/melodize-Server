fd97cffb3031b5bc3ae997d4f94ac1413d2855a2
RefaelBZ <refael747@gmail.com>
2024-09-12 22:02:42 +0300

    fix user log in

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-
 const db = require("./db");
 db.connect();
 
 const cors = require('cors');
+
+// Apply CORS middleware before defining routes
 app.use(cors({
   origin: ['http://localhost:5173', 'https://flourishing-dusk-595691.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
 app.use(express.json());
+
+// Handle preflight requests
+app.options('*', cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
@@ -27,12 +33,11 @@ app.use('/top', topRouter);
 const craftedRouter = require('./Crafted/Crafted.router');
 app.use('/crafted', craftedRouter);
 
-const trendingdRouter = require('./Trending/Trending.router');
-app.use('/trending', trendingdRouter);
+const trendingRouter = require('./Trending/Trending.router');
+app.use('/trending', trendingRouter);
 
 const userRouter = require('./User/user.router')
 app.use("/user", userRouter)
-
 
 const PORT = process.env.PORT || 2500;
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}!`));
